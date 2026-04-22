e2a7af621fc1f16a7465d14ed5642aeb642693a1
Xojiakbarov Abduvoris <abduvoris.2025@gmail.com>
2026-04-22 19:18:04 +0500

    men port ulanishni ozgartirdim

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: ['localhost:3000', 'https://mironshox.uz'] });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000, ()=>{
+    console.log(`Server is running ... on ${process.env.PORT || 4000 }`)
+  });
 }
 bootstrap();
